21edb4aa47fc187fbb651100b98caa757c51a462
Jaime Moreno Cantó <jmc103@alu.ua.es>
2019-01-24 21:18:25 +0100

    GraphQL initial configuration and schema

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 import express from 'express'
+// GraphQL
+import graphqlHTTP from 'express-graphql'
+import schema from './schema'
 
 const app = express()
 
 app.get('/', (req, resp) => {
   res.send('All ready')
 })
+
+// GraphQL resolver
+const root = { hello: () => { return "Hello World from GraphQL" } }
+
+app.use('/graphql', graphqlHTTP({
+  schema,
+  rootValue: root,
+  graphiql: true
+}))
 
 app.listen(8000, () => {
   console.log('Server running...')

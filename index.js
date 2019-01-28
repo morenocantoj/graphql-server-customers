9aa18b831b99c41daecfd188a2afd7c5cdf2f7f0
Jaime Moreno Cantó <jmc103@alu.ua.es>
2019-01-28 16:53:41 +0100

    Migration to Apollo Server

--- index.js
+++ index.js
@@ -1,19 +1,12 @@
 import express from 'express'
 // GraphQL
-import graphqlHTTP from 'express-graphql'
-import { schema } from './data/schema'
+import { ApolloServer } from 'apollo-server-express'
+import { typeDefs } from './data/schema'
+import { resolvers } from './data/resolvers'
 
 const app = express()
+const server = new ApolloServer({typeDefs, resolvers})
 
-app.get('/', (req, resp) => {
-  res.send('All ready')
-})
+server.applyMiddleware({app})
 
-app.use('/graphql', graphqlHTTP({
-  schema,
-  graphiql: true
-}))
-
-app.listen(8000, () => {
-  console.log('Server running...')
-})
+app.listen({port: 4000}, () => console.log("Server running... http://localhost:4000" + server.graphqlPath))
